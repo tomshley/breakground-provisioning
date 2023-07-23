7216057fc1df450e265c10643e9de341f8c8c58a
sgoggles <gitlab.comsgogglespm.coil197@passmail.net>
2023-07-23 14:29:10 -0400

    tomshley/breakground-provisioning#6
    https://gitlab.com/tomshley/breakground-provisioning/-/issues/6

--- scripts/cicd-bootstrap-gitlab.sh
+++ scripts/cicd-bootstrap-gitlab.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 # shellcheck source=cicd-exports.sh
-. ./cicd-exports.sh
+. "${CI_REPOSITORY_URL}/scripts/cicd-exports.sh"
 
 cd "${TF_ROOT}" || exit
 curl --silent "https://gitlab.com/gitlab-org/incubation-engineering/mobile-devops/download-secure-files/-/raw/main/installer" | bash
