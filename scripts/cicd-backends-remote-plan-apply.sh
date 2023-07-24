12c51c1f526aea246d0468c530606a9d5b682fa4
sgoggles <gitlab.comsgogglespm.coil197@passmail.net>
2023-07-23 21:15:15 -0400

    tomshley/breakground-provisioning#6
    https://gitlab.com/tomshley/breakground-provisioning/-/issues/6

--- scripts/cicd-backends-remote-plan-apply.sh
+++ scripts/cicd-backends-remote-plan-apply.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
-. "${CI_PROJECT_DIR}/scripts/cicd-bootstrap-gitlab-terraform.sh"
-cd "${TF_BACKENDS_REMOTE_ROOT}" || exit
+. "${CI_PROJECT_DIR}/scripts/cicd-bootstrap-gitlab-terraform-remote.sh"
 make apply
