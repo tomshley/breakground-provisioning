2822e232fac2bf26fc95b8872f6450f1e03d50eb
sgoggles <gitlab.comsgogglespm.coil197@passmail.net>
2023-07-25 18:04:05 -0400

    tomshley/breakground-provisioning#6
    https://gitlab.com/tomshley/breakground-provisioning/-/issues/6

---
+++ scripts/cicd-flow-release-start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+cd "${CI_PROJECT_DIR}" || exit
+
+# set the -sbt- build to release not, snapshot
+tomshley_project_version_src="${CI_PROJECT_DIR}/VERSION"
+
+git fetch
+git checkout -b "release/${TOMSHLEY_BREAKGROUND_BUILD_VERSION_NEXT}" develop
+
+echo "${TOMSHLEY_BREAKGROUND_BUILD_VERSION_NEXT}" > "${tomshley_project_version_src}"
+
+git add "${tomshley_project_version_src}"
+
+git commit -m "$(git log --format='%B' -n 1) | bumping version to ${TOMSHLEY_BREAKGROUND_BUILD_VERSION_NEXT}"
