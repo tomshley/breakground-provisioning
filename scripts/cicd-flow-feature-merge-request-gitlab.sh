4f67dbb0a06d90cbb438f826d09b95141c1425a6
sgoggles <gitlab.comsgogglespm.coil197@passmail.net>
2023-07-30 11:29:22 -0400

    tomshley/breakground-provisioning#6
    https://gitlab.com/tomshley/breakground-provisioning/-/issues/6
    
    debug

--- scripts/cicd-flow-feature-merge-request-gitlab.sh
+++ scripts/cicd-flow-feature-merge-request-gitlab.sh
@@ -21,10 +21,10 @@
 cd "${CI_PROJECT_DIR}" || exit
 
 ## credit: https://about.gitlab.com/blog/2017/09/05/how-to-automatically-create-a-new-mr-on-gitlab-with-gitlab-ci/
-[[ $HOST =~ ^https?://[^/]+ ]] && HOST="${BASH_REMATCH[0]}/api/v4/projects/"
+[[ $CI_PROJECT_URL =~ ^https?://[^/]+ ]] && CI_PROJECT_URL="${BASH_REMATCH[0]}/api/v4/projects/"
 
 # Look which is the default branch
-#TARGET_BRANCH=`curl --silent "${HOST}${CI_PROJECT_ID}" --header "PRIVATE-TOKEN:${PRIVATE_TOKEN}" | python3 -c "import sys, json; print(json.load(sys.stdin)['default_branch'])"`;
+#TARGET_BRANCH=`curl --silent "${CI_PROJECT_URL}${CI_PROJECT_ID}" --header "PRIVATE-TOKEN:${PRIVATE_TOKEN}" | python3 -c "import sys, json; print(json.load(sys.stdin)['default_branch'])"`;
 TARGET_BRANCH="develop"
 
 # The description of our new MR, we want to remove the branch after the MR has
@@ -41,16 +41,16 @@ BODY="{
 echo "BODY: ${BODY}"
 # Require a list of all the merge request and take a look if there is already
 # one with the same source branch
-#LISTMR=`curl --silent "${HOST}${CI_PROJECT_ID}/merge_requests?state=opened" --header "PRIVATE-TOKEN:${PRIVATE_TOKEN}"`;
-echo "${HOST}${CI_PROJECT_ID}/merge_requests?state=opened --header PRIVATE-TOKEN:${PRIVATE_TOKEN}";
-LISTMR=`curl "${HOST}${CI_PROJECT_ID}/merge_requests?state=opened" --header "PRIVATE-TOKEN:${PRIVATE_TOKEN}"`;
+#LISTMR=`curl --silent "${CI_PROJECT_URL}${CI_PROJECT_ID}/merge_requests?state=opened" --header "PRIVATE-TOKEN:${PRIVATE_TOKEN}"`;
+echo "${CI_PROJECT_URL}${CI_PROJECT_ID}/merge_requests?state=opened --header PRIVATE-TOKEN:${PRIVATE_TOKEN}";
+LISTMR=`curl "${CI_PROJECT_URL}${CI_PROJECT_ID}/merge_requests?state=opened" --header "PRIVATE-TOKEN:${PRIVATE_TOKEN}"`;
 echo "LISTMR: ${LISTMR}"
 COUNTBRANCHES=`echo ${LISTMR} | grep -o "\"source_branch\":\"${CI_COMMIT_REF_NAME}\"" | wc -l`;
 echo "COUNTBRANCHES: ${COUNTBRANCHES}"
 
 # No MR found, let's create a new one
 if [ ${COUNTBRANCHES} -eq "0" ]; then
-    curl -X POST "${HOST}${CI_PROJECT_ID}/merge_requests" \
+    curl -X POST "${CI_PROJECT_URL}${CI_PROJECT_ID}/merge_requests" \
         --header "PRIVATE-TOKEN:${PRIVATE_TOKEN}" \
         --header "Content-Type: application/json" \
         --data "${BODY}";
