448abb012ab9b21ef203ddab6deb09e2d52ef271
sgoggles <gitlab.comsgogglespm.coil197@passmail.net>
2023-07-30 10:40:56 -0400

    tomshley/breakground-provisioning#6
    https://gitlab.com/tomshley/breakground-provisioning/-/issues/6

--- scripts/cicd-dockers-build.sh
+++ scripts/cicd-dockers-build.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env sh
+#
+# Copyright 2023 Tomshley LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# @author Thomas Schena @sgoggles <https://github.com/sgoggles> | <https://gitlab.com/sgoggles>
+#
 . "${CI_PROJECT_DIR}/scripts/cicd-bootstrap-gitlab.sh"
 cd "${DOCKERS_LOCAL_ROOT}" || exit
 make build
