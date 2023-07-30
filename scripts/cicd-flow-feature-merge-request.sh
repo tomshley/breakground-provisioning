fbef70d5631d931797b7053fd65dd96ccf094519
sgoggles <gitlab.comsgogglespm.coil197@passmail.net>
2023-07-30 11:24:53 -0400

    tomshley/breakground-provisioning#6
    https://gitlab.com/tomshley/breakground-provisioning/-/issues/6
    
    debug

--- scripts/cicd-flow-feature-merge-request.sh
+++ scripts/cicd-flow-feature-merge-request.sh
@@ -43,8 +43,8 @@ echo "BODY: ${BODY}"
 # one with the same source branch
 #LISTMR=`curl --silent "${HOST}${CI_PROJECT_ID}/merge_requests?state=opened" --header "PRIVATE-TOKEN:${PRIVATE_TOKEN}"`;
 LISTMR=`curl "${HOST}${CI_PROJECT_ID}/merge_requests?state=opened" --header "PRIVATE-TOKEN:${PRIVATE_TOKEN}"`;
-COUNTBRANCHES=`echo ${LISTMR} | grep -o "\"source_branch\":\"${CI_COMMIT_REF_NAME}\"" | wc -l`;
 echo "LISTMR: ${LISTMR}"
+COUNTBRANCHES=`echo ${LISTMR} | grep -o "\"source_branch\":\"${CI_COMMIT_REF_NAME}\"" | wc -l`;
 echo "COUNTBRANCHES: ${COUNTBRANCHES}"
 
 # No MR found, let's create a new one
