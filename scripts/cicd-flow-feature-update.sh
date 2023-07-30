604ab081d6570ac5e95d809476fc25bea0473306
sgoggles <gitlab.comsgogglespm.coil197@passmail.net>
2023-07-30 09:57:02 -0400

    tomshley/breakground-provisioning#6
    https://gitlab.com/tomshley/breakground-provisioning/-/issues/6

---
+++ scripts/cicd-flow-feature-update.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+. "${CI_PROJECT_DIR}/scripts/cicd-exports.sh"
+
+cd "${CI_PROJECT_DIR}" || exit
+
+git fetch
+git checkout -b "release/${TOMSHLEY_BREAKGROUND_BUILD_VERSION_NEXT}" develop
+
+echo "${TOMSHLEY_BREAKGROUND_BUILD_VERSION_NEXT}" > "${tomshley_project_version_src}"
+
+git add "${tomshley_project_version_src}"
+
+git commit -m "$(git log --format='%B' -n 1) | bumping version to ${TOMSHLEY_BREAKGROUND_BUILD_VERSION_NEXT}"
